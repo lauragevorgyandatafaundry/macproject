4482cc9628eeb4b4b1ca188471673bf38a3fcf77
ashkhen nersisyan <ashkhen.nersisyan@powerdata.de>
2021-08-22 17:27:42 +0400

    added getByname api

---
+++ src/backend/api/getByName.php
@@ -0,0 +1,40 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../config/database.php';
+include_once '../class/Community.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$item = new Community($db);
+$item->db_table = "vernashen_churches";
+
+$item->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+
+$item->getChurcheById();
+
+if ($item->name != null) {
+    // create array
+    $emp_arr = array(
+        "id" => $item->id,
+        "name" => $item->name,
+//        "blind" => $item->blind,
+//        "alley" => $item->alley,
+//        "house" => $item->house,
+        "latitude" => $item->latitude,
+        "longitude" => $item->longitude
+    );
+
+    http_response_code(200);
+    echo json_encode($emp_arr);
+} else {
+    http_response_code(404);
+    echo json_encode("Not found.");
+}
+?>
--- src/backend/class/Community.php
+++ src/backend/class/Community.php
@@ -1,43 +1,55 @@
 <?php
-    class Community
+
+class Community
+{
+
+    private $conn;
+
+    public $db_table = "";
+//
+    public $id;
+//        public $blind;
+//        public $alley;
+//        public $house;
+//        public $latitude;
+//        public $longitude;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function getCommunityData()
+    {
+        $sqlQuery = "SELECT id,street, blind, alley, house, latitude, longitude FROM " . $this->db_table;
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function getChurches()
+    {
+        $sqlQuery = "SELECT id,name, latitude, longitude FROM " . $this->db_table;
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->execute();
+        return $stmt;
+    }
+
+
+    public function getChurcheById()
     {
 
-        private $conn;
+        $sqlQuery = "SELECT id,name ,latitude, longitude FROM " . $this->db_table . " WHERE  name = ?";
 
-        public $db_table = "";
+        $stmt = $this->conn->prepare($sqlQuery);
 
-        public $street;
-        public $blind;
-        public $alley;
-        public $house;
-        public $latitude;
-        public $longitude;
+        $stmt->bindParam(1, $this->id);
 
-        public function __construct($db)
-        {
-            $this->conn = $db;
-        }
+        $stmt->execute();
 
-        public function getCommunityData()
-        {
-            $sqlQuery = "SELECT id,street, blind, alley, house, latitude, longitude FROM " . $this->db_table;
-            $stmt = $this->conn->prepare($sqlQuery);
-            $stmt->execute();
-            return $stmt;
-        }
-        public function getChurches()
-        {
-            $sqlQuery = "SELECT id,name, latitude, longitude FROM " . $this->db_table;
-            $stmt = $this->conn->prepare($sqlQuery);
-            $stmt->execute();
-            return $stmt;
-        }
-
-
-        public function getSingleEmployee()
-        {
-            $sqlQuery = "SELECT street,blind,alley,house,latitude,longitude FROM" . $this->db_table . "WHERE id = ? LIMIT 0,1";
-
+        $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (empty($dataRow)) {
+            $sqlQuery = "SELECT id,name ,latitude, longitude FROM  vernashen_tourist_places WHERE  name = ?";
             $stmt = $this->conn->prepare($sqlQuery);
 
             $stmt->bindParam(1, $this->id);
@@ -45,13 +57,15 @@
             $stmt->execute();
 
             $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            $this->name = $dataRow['name'];
-            $this->email = $dataRow['email'];
-            $this->age = $dataRow['age'];
-            $this->designation = $dataRow['designation'];
-            $this->created = $dataRow['created'];
         }
 
+
+        $this->id = $dataRow['id'];
+        $this->name = $dataRow['name'];
+        $this->latitude = $dataRow['latitude'];
+        $this->longitude = $dataRow['longitude'];
+
     }
+
+}
 
